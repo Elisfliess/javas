4ff05d805200f19f798291d27b188f1d802b0e00
ElisFliess <102757856+Elisfliess@users.noreply.github.com>
2022-05-04 16:51:19 -0300

    Add files via upload

---
+++ 1-codigo.js
@@ -0,0 +1,23 @@
+console.log('JS por Lise');
+console.log(7+4);
+console.log('7'+'4');
+console.log(12+20);
+console.log(25+157);
+console.log(30+16);
+
+console.log('Lise' + 'Fliess');
+
+
+
+const nome ='Lise';
+console.log(nome);
+
+const a = 2;
+const b = 3;
+
+console.log(a+b);
+
+const  c = 1500;
+const  d = 1200;
+
+console.log(c+d);
